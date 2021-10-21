5f5cd6a2af1e6bc32d9ea32b87550d710c0bb11e
Xawe <dreamxawe@hotmail.com>
2021-10-21 11:38:29 -0300

    cleaning

--- lib/simple.ex
+++ lib/simple.ex
@@ -12,25 +12,10 @@ defmodule Simple do
   end
 
 
-  def handle_call(:c, from, _state) do
-    {:reply, "Hello", from}
-  end
-
-  def handle_call({:d, value}, from, _state) do
-    {:reply, value, from}
-  end
-
   def handle_cast({:e, value}, _state \\[]) do
     {:noreply, "#{value} >> OK"}
   end
 
-  def c(value) do
-    GenServer.call(__MODULE__, {:c, value})
-  end
-
-  def d(value) do
-    GenServer.call(__MODULE__, {:d, value})
-  end
 
   def e(value, state \\ []) do
     IO.inspect("Starting")
@@ -45,23 +30,5 @@ defmodule Simple do
     r
   end
 
-  def one(value) do
-    {_r, id} = DynamicSupervisor.start_child(DynamicCaller, Simple)
-    IO.inspect(value)
-    result = GenServer.call(id, {:c, value})
-    IO.inspect("===== #{result}")
-  end
-
-  def two(value, at) do
-    {_r, id} = DynamicSupervisor.start_child(DynamicCaller, Simple)
-    IO.inspect(id)
-    IO.inspect(Process.alive?(id))
-    r = GenServer.call(id, {:d, value})
-    Process.exit(id, :sucess)
-    IO.inspect(Process.alive?(id))
-    IO.inspect(at)
-    r
-
-  end
 
 end
